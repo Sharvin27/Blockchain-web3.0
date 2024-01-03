2b68a4a4ca3c32726733c6077969df61712a6bad
Sharvin Gavad <112688310+Sharvin27@users.noreply.github.com>
2024-01-03 20:58:05 +0530

    Update constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xAA92D909BdD3E1841B65AA12A84aB7Dd7487DD7d";
+export const contractAddress = "Contract Address";
 export const contractABI = abi.abi;
